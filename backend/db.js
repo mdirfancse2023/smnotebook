05cc4416a5642cb359c5503b037d6866de9c8d92
Irfan <mdirfancse2023@gmail.com>
2023-02-21 17:51:11 +0530

    17th Commit

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,7 @@
 // Use to connect with database
 const mongoose = require('mongoose');
-const mongouri = 'mongodb://localhost:27017/inotebook';
+const mongouri = 'mongodb+srv://inotebook:connect@inotebook.zrpvbmj.mongodb.net/test'
+//const mongouri = 'mongodb://localhost:27017/inotebook';
 const connectToMongo = async ()=>{
     await mongoose.connect(mongouri);
 }
